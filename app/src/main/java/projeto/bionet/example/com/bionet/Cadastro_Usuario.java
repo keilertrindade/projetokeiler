7691dd0228714dee036acd6836dfcf3d303802c8
keilertrindade <31934263+keilertrindade@users.noreply.github.com>
2017-09-29 16:41:16 -0300

    Layout
    Inicio de teste para troca de campos especificos para preenchimento de cadastro de pessoa fisica ou juridica.

--- app/src/main/java/projeto/bionet/example/com/bionet/Cadastro_Usuario.java
+++ app/src/main/java/projeto/bionet/example/com/bionet/Cadastro_Usuario.java
@@ -2,12 +2,31 @@ package projeto.bionet.example.com.bionet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class Cadastro_Usuario extends AppCompatActivity {
+
+    RadioGroup radioGroup =(RadioGroup)findViewById(R.id.radiogroup);
+    final EditText et1 = (EditText) findViewById(R.id.nome);
+    final EditText et2 = (EditText) findViewById(R.id.razao);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro__usuario);
     }
-}
+
+     //Posso dar loading com todos os campos invisiveis, carregando-os apenas após escolher o rbuuton
+
+    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+            if(checkedId == R.id.p_fisica){
+                et1.setVisibility(View.VISIBLE);
+                et2.setVisibility(View.INVISIBLE);
+            } else {
+                et1.setVisibility(View.INVISIBLE);
+                et2.setVisibility(View.VISIBLE);
+            }
+        }
+    }
