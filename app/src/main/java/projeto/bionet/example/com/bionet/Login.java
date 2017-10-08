c0579599366f4cfc759e0f1dc5febe35275f3477
keilertrindade <31934263+keilertrindade@users.noreply.github.com>
2017-10-08 15:15:09 -0300

    Firebase
    Tentativa de implementação de login com o Firebase, porém aplicação fecha ao chamar o metodo de login.

--- app/src/main/java/projeto/bionet/example/com/bionet/Login.java
+++ app/src/main/java/projeto/bionet/example/com/bionet/Login.java
@@ -1,31 +1,94 @@
 package projeto.bionet.example.com.bionet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     private Button botao;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText edtEmail, edtSenha;
+    String email, senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        mAuth = FirebaseAuth.getInstance();
 
+        edtSenha = (EditText) findViewById(R.id.senha);
+        edtEmail = (EditText) findViewById(R.id.email);
         botao = (Button) findViewById(R.id.botao_registro);
+        email = edtEmail.getText().toString() ;
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(getApplicationContext(), "Bem Vindo " + user.getDisplayName() + "!",
+                            Toast.LENGTH_LONG).show();
+                    Intent it = new Intent(getApplicationContext(),LobbyActivity.class);
+                    startActivity(it);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Sem Usuários Conectados!",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+    }
+
+    public void clickLogin(View v) {
+
+        mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (!task.isSuccessful()) {
+                    Log.w("AUTH", "Falha ao efetuar o Login: ", task.getException());
+                }else{
+                    Log.d("AUTH", "Login Efetuado com sucesso!!!");
+                }
+            }
+        });
     }
 
 
-    public void onClick (View v) {
-
+    public void Cadastro (View v) {
         Intent intent = new Intent(Login.this,Cadastro_Usuario.class);
         startActivity(intent);
-
-        //Your Logic
     }
 
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 }
+
