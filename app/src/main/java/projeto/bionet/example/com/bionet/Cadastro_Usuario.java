631f812e3b9de823720f10ecb259c2faff2f8208
keilertrindade <31934263+keilertrindade@users.noreply.github.com>
2017-10-12 14:03:54 -0300

    Firebase
    Implementação de código básico para cadastro e inclusão de dados na databasde, porém nenhum funciona corretamente ainda.
    
    Adição de campo para inclusão de número no endereço do cadastro de pessoa fisica

--- app/src/main/java/projeto/bionet/example/com/bionet/Cadastro_Usuario.java
+++ app/src/main/java/projeto/bionet/example/com/bionet/Cadastro_Usuario.java
@@ -1,6 +1,7 @@
 package projeto.bionet.example.com.bionet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,23 +9,45 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import static android.support.design.R.styleable.TextInputLayout;
 
 public class Cadastro_Usuario extends AppCompatActivity {
 
-   // RadioGroup grupo = (RadioGroup) findViewById(R.id.radiogroup);
-   // EditText et1 = (EditText) findViewById(R.id.nome);
-   // EditText et2 = (EditText) findViewById(R.id.razao);
-   RadioGroup RGrupo;
+    private FirebaseAuth mAuth;
+
+    private EditText etEmail, etSenha, etNome, etSnome, etCpf, etCep, etRua, etBairro, etCidade, etEstado;
+    private String email, senha, nome, snome, cpf, cep, rua, bairro, cidade, estado;
+
+
+    RadioGroup RGrupo;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_cadastro__usuario);
         RGrupo = (RadioGroup) findViewById(R.id.radiogroup);
+        mAuth = FirebaseAuth.getInstance();
+
+        etEmail = (EditText) findViewById(R.id.email);
+        etSenha = (EditText) findViewById(R.id.password);
+        etNome = (EditText) findViewById(R.id.nome);
+        etSnome = (EditText) findViewById(R.id.sobrenome);
+        etCpf = (EditText) findViewById(R.id.cpf);
+        etCep = (EditText) findViewById(R.id.cep);
+        etRua = (EditText) findViewById(R.id.rua);
+        etCidade = (EditText) findViewById(R.id.cidade);
+        etBairro = (EditText) findViewById(R.id.bairro);
+        etEstado = (EditText) findViewById(R.id.estado);
 
 
         RGrupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -42,5 +65,48 @@ public class Cadastro_Usuario extends AppCompatActivity {
             }
         });
     }
+
+
+    public void Cadastrar(View view){
+
+
+        email = etEmail.getText().toString().trim();
+        senha = etSenha.getText().toString().trim();
+        nome = etNome.getText().toString().trim();
+        snome = etSnome.getText().toString().trim();
+        cpf = etCpf.getText().toString().trim();
+        cep = etCep.getText().toString().trim();
+        rua = etRua.getText().toString().trim();
+        bairro = etBairro.getText().toString().trim();
+        cidade = etCidade.getText().toString().trim();
+        estado = etEstado.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Cadastro_Usuario.this, "Erro ao criar usuário!",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            DatabaseReference myRef = database.getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                           // myRef.child("uid").setValue(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            myRef.child("cpf").setValue(cpf);
+                            myRef.child("nome").setValue(nome);
+                            myRef.child("sobrenome").setValue(snome);
+                            myRef.child("email").setValue(email);
+
+                            myRef.child("cep").setValue(cep);
+                            myRef.child("rua").setValue(rua);
+                            myRef.child("bairro").setValue(bairro);
+                            myRef.child("cidade").setValue(cidade);
+                            myRef.child("estado").setValue(estado);
+                        }
+                    }
+                });
+
     }
+}
 
