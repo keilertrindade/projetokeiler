23882fbb062fe965f11ac7b05242277169910452
Keiler Trindade <keiler.trindade@hotmail.com>
2017-10-16 13:36:42 -0300

    Firebase - Cadastro Pessoa Juridica
    - Adcionada funções para realização do cadastro de credencias, login, cadastro de informações de perfil na Activity cadastroPJuridica.
    
    - Função testada e totalmente funcional.

--- app/src/main/java/projeto/bionet/example/com/bionet/cadastroPJuridica.java
+++ app/src/main/java/projeto/bionet/example/com/bionet/cadastroPJuridica.java
@@ -1,11 +1,38 @@
 package projeto.bionet.example.com.bionet;
 
 import android.content.Intent;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class cadastroPJuridica extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    FirebaseFirestore db;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText etEmail, etSenha, etRazao, etCnpj, etCep, etRua, etNum, etBairro, etCidade, etEstado;
+    private String email, senha, razao, cnpj, cep, rua, num, bairro, cidade, estado, teste;
+    private Address retornoCep;
 
     RadioGroup RGrupo;
 
@@ -15,6 +42,22 @@ public class cadastroPJuridica extends AppCompatActivity {
         // overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         setContentView(R.layout.activity_cadastro_pjuridica);
         RGrupo = (RadioGroup) findViewById(R.id.radiogroup);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        db = FirebaseFirestore.getInstance();
+        teste = "";
+
+
+        etEmail = (EditText) findViewById(R.id.email);
+        etSenha = (EditText) findViewById(R.id.password);
+        etRazao = (EditText) findViewById(R.id.razao);
+        etCnpj = (EditText) findViewById(R.id.cnpj);
+        etCep = (EditText) findViewById(R.id.cep);
+        etRua = (EditText) findViewById(R.id.rua);
+        etNum = (EditText) findViewById(R.id.num);
+        etCidade = (EditText) findViewById(R.id.cidade);
+        etBairro = (EditText) findViewById(R.id.bairro);
+        etEstado = (EditText) findViewById(R.id.estado);
 
         RGrupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 
@@ -33,4 +76,127 @@ public class cadastroPJuridica extends AppCompatActivity {
 
 
     }
+
+
+    public void checarCampos(View v){
+
+        email = etEmail.getText().toString().trim();
+        senha = etSenha.getText().toString().trim();
+        razao = etRazao.getText().toString().trim();
+        cnpj = etCnpj.getText().toString().trim();
+        cep = etCep.getText().toString().trim();
+        rua = etRua.getText().toString().trim();
+        num = etNum.getText().toString().trim();
+        bairro = etBairro.getText().toString().trim();
+        cidade = etCidade.getText().toString().trim();
+        estado = etEstado.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)) {
+            etEmail.setError("O campo Email deve ser preenchido!");
+            etEmail.requestFocus();
+            return;
+        }
+        else if (TextUtils.isEmpty(senha)){
+            etSenha.setError("O campo Senha deve ser preenchido!");
+            etSenha.requestFocus();
+            return;
+        }
+        else if (TextUtils.isEmpty(razao)){
+            etRazao.setError("O campo Razão Social deve ser preenchido!");
+            etRazao.requestFocus();
+            return;
+        }
+
+        else if (TextUtils.isEmpty(cnpj) || cnpj.length() < 14){
+            etCnpj.setError("O campo CNPJ deve ser preenchido corretamente!");
+            etCnpj.requestFocus();
+            return;
+        }
+        else if (TextUtils.isEmpty(cep) || cep.length() < 8){
+            etCep.setError("O campo CEP deve ser preenchido corretamente!");
+            etCep.requestFocus();
+            return;
+        }
+        // Talvez esses próximos não sejam necessários tendo em vista a checagem do CEP
+        else if (TextUtils.isEmpty(rua)){
+            etRua.setError("O campo Rua deve ser preenchido!");
+            return;
+        }
+        else if (TextUtils.isEmpty(bairro)){
+            etBairro.setError("O campo Bairro deve ser preenchido!");
+            return;
+        }
+        else if (TextUtils.isEmpty(cidade)){
+            etCidade.setError("O campo Cidade deve ser preenchido!");
+            return;
+        }
+        else if (TextUtils.isEmpty(estado)){
+            etEstado.setError("O campo Estado deve ser preenchido!");
+            return;
+        }
+        else{
+            Cadastrar(/*email,senha,nome,snome,cpf,cep,rua,num,bairro,cidade,estado*/);
+        }
+
+
+    }
+
+    public void Cadastrar(/*String email, String senha, String nome, String snome, String cpf, String cep,
+                          String rua, String num, String bairro, String cidade, String estado */){
+        mAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(cadastroPJuridica.this, "Erro ao criar usuário!",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+
+                            Toast.makeText(cadastroPJuridica.this, "Usuário Criado com sucesso!",
+                                    Toast.LENGTH_LONG).show();
+
+                            salvarPerfil();
+                        }
+                    }
+                });
+    }
+
+    public void salvarPerfil(){
+
+        mAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(cadastroPJuridica.this, "Erro ao salvar informações - LOGAR 2!",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            String id = user.getUid();
+
+                            Map<String, Object> usuario = new HashMap<>();
+                            usuario.put("Razão Social", razao);
+                            usuario.put("cnpj", cnpj);
+                            usuario.put("cep", cep);
+                            usuario.put("rua", rua);
+                            usuario.put("numero", num);
+                            usuario.put("bairro", bairro);
+                            usuario.put("cidade", cidade);
+                            usuario.put("estado", estado);
+                            usuario.put("tipo","Pessoa Juridíca");
+
+                            db.collection("Profile").document(id).set(usuario);
+                            SystemClock
+
+
+
+                            Intent intent = new Intent(cadastroPJuridica.this, Login.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+    }
+
 }
