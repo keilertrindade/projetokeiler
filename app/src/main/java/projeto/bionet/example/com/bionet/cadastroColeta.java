bde272139a798da9cf3fdfa090aed261137297b9
Keiler Trindade <keiler.trindade@hotmail.com>
2017-10-18 16:54:36 -0300

    Cadastro Coleta
    Criação da activity cadastroColeta com cópia do layout da activity cadastropfisica

---
+++ app/src/main/java/projeto/bionet/example/com/bionet/cadastroColeta.java
@@ -0,0 +1,13 @@
+package projeto.bionet.example.com.bionet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastroColeta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_coleta);
+    }
+}
